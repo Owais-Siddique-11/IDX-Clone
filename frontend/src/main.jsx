f41f6227c2e1b71ee9ebd10181b5fd09b5ef2d25
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2025-09-04 20:42:02 +0530

    Added react query for api fetching

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import './index.css'
+import { QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import App from './App.jsx'
-
+const queryClient=new QueryClient();
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+<BrowserRouter>
+ <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientProvider>
+ </BrowserRouter>
 )
